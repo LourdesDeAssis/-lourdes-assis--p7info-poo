0493eb561e17a241c500ef7cf1001bdb29a59d71
LourdesDeAssis <100160739+LourdesDeAssis@users.noreply.github.com>
2022-05-15 19:42:12 -0300

    Update atividade-presencial-3.ts

--- atividade-pre/atividade-presencial-3.ts
+++ atividade-pre/atividade-presencial-3.ts
@@ -1,83 +1,67 @@
-/* DECIDI USAR POO PARA FAZER O EXERCÍCIO POR CONTA
-DE TER COMO INSTANCIAR UM OBJETO, PODENDO CRIAR PILHAS DIFERENTES*/
-
-//Criando a classe
 class Stack{
-    // Passando os atributos dessa classe
+    
     stack: number[] = []
-    topo: number = -1
-    maxStack = 4
+    topoDaPilha: number = -1
+    max = 4
 
-    // Metódo para verificar se a Pilha está vazia
-    isEmpty(): number{
-        if(this.topo == -1){
+    vazio(): number{
+        if(this.topoDaPilha == -1){
             return 1
         }else{
             return 0
         }
     }
-    // Metódo para verificar se a Pilha está cheia
-    isFull(): number{
-        if(this.topo == this.maxStack){
+    cheio(): number{
+        if(this.topoDaPilha == this.max){
             return 1
         }else{
             return 0
         }
     }
-    // Metódo para inserir elementos na Pilha
     push(num: number): void{
-        if(this.isFull() == 1){
-            console.log('Pilha cheia')
+        if(this.cheio() == 1){
+            console.log('A pilha se encontra cheia')
         }else{
-            this.topo++
-            this.stack[this.topo] = num
-            console.log('Elemento '+num+' Adicionado!')
+            this.topoDaPilha++
+            this.stack[this.topoDaPilha] = num
+            console.log(+num+' se encontra acrestado à lista')
         }
     }
-    // Metódo para remover elementos na Pilha
     pop(): number | void{
-        if(this.isEmpty() == 1){
-            console.log('Pilha vazia')
+        if(this.vazio() == 1){
+            console.log('A pilha se encontra vazia')
         }else{
-            let aux = this.stack[this.topo]
-            console.log('Elemento '+aux+' Retirado')
-            this.topo--
+            let aux = this.stack[this.topoDaPilha]
+            console.log(+aux+' ')
+            this.topoDaPilha--
             return aux
         }
     }
-    // Metódo para mostrar a Pilha
     view(): void{
-        if(this.isEmpty() == 1){
-            console.log('Pilha Vazia')
+        if(this.vazio() == 1){
+            console.log('A pilha se encontra vazia')
          }else{
-            for(let i = 0; i <= this.topo; i++){
-                console.log('Pilha tem o elemento '+this.stack[i])    
+            for(let i = 0; i <= this.topoDaPilha; i++){
+                console.log('A pilha apresenta o  elemento '+this.stack[i])    
             }
         }
     }
 }
-// Criando um objeto do tipo Stack
-let pilha: Stack = new Stack()
 
-// Inserindo elementos na Pilha
+let pilha: Stack = new Stack()
 pilha.push(1)
 pilha.push(2)
 pilha.push(3)
 pilha.push(4)
 console.log('-------------------------')
-// Mostrando a Pilha (Possui 4 elementos)
 pilha.view()
 console.log('-------------------------')
-// Removendo 2 elementos da Pilha
 pilha.pop()
 pilha.pop()
 console.log('-------------------------')
-// Mostrando a Pilha (Possui 2 elementos)
 pilha.view()
 console.log('-------------------------')
-// Removendo 2 elementos da Pilha
 pilha.pop()
 pilha.pop()
 console.log('-------------------------')
-// Mostrando a pilha (Vazia)
 pilha.view()
