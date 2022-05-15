a7a564668a5d5757246fd87b2f8e3c22886706e8
LourdesDeAssis <100160739+LourdesDeAssis@users.noreply.github.com>
2022-05-15 19:59:58 -0300

    Add files via upload

---
+++ atividade-pre/atividade-presencial-5.ts
@@ -0,0 +1,54 @@
+/*Domínio do problema -> Estabelecimento de venda de carros
+
+Entidades
+
+- Vendedor
+- Cliente
+- Carro
+- Fornecedor
+
+Abaixo estão as classes com seus devidos atributos e metodos
+
+- Classe Carro -
+Atributos:
+
+- Nome
+- Marca
+- Ano
+- Cor
+
+Métodos:
+
+- cadastroDoNome
+- cadastrpDaMarcar
+- cadastroDoAno
+- cadastroDaCor
+
+- Classe Fornecedor -
+Atributos:
+
+- Nome
+- CNPJ
+- CarrosVendidos
+
+Métodos:
+
+- cadastroDoNome
+- cadastroDosCarrosVendidos
+
+
+- Classe Pessoa -
+Atributos:
+
+- Nome
+- CPF
+- Telefone
+- Cargo
+
+Métodos
+
+- cadastroDoNome
+- cadastroDoCPF
+- cadastroDoTelefone
+- cadastroDoCargo
+\*
