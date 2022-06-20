e95f145e3845c99de995010840e9e80eea399c22
LourdesDeAssis <100160739+LourdesDeAssis@users.noreply.github.com>
2022-06-20 17:06:22 -0300

    Update nota_fiscal.ts

--- NotaFiscal/nota_fiscal.ts
+++ NotaFiscal/nota_fiscal.ts
@@ -49,13 +49,13 @@ export class NotaFiscal {
 
     adicionarItem(item: ItemNotaFiscal) {
          this.items.push(item);
+        this.valorNota += item.valor;
     }
 
 
     calcularValorNotaFiscal() : number {
         let valorCalculado = 0;
-
-
+        
         return valorCalculado;
 
     }
@@ -63,7 +63,24 @@ export class NotaFiscal {
     // Para apresentar a nota fiscal conforme as orientações 
     
     imprimirNotaFiscal(): void {
-
+        
+    var data = new Date();
+    console.log("--------------------------------------------------------------------------------------------------------------------")
+    console.log("NOTA FISCAL                                                                                                   ", data)
+    console.log("Cliente:",this.cliente.getcodigo(), "      Nome:",this.cliente.getnome() )
+    console.log("CPF:",this.cliente.getcnpjcpf())
+    console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    console.log("ITENS")
+    console.log("--------------------------------------------------------------------------------------------------------------------")
+    console.log("Seq    descrição                                                               QTD        Preco UNIT        Preço   ")
+    console.log("------------------------------------------------------------------------      -----     -------------     ----------")
+    for(let count = 0; count < this.items.length; count++){
+        let precoTotal =  this.items[count].getquant() * this.items[count].produto.getprecoUnico()
+        this.precoTotal += precoTotal
+        console.log("%i       %s                                                       %f            %f              %f  ",
+        this.items[count].getsequencial(), this.items[count].produto.getdescricao(), this.items[count].getquant(), this.items[count].produto.getprecoUnico(), precoTotal)
+    }
+    console.log("")
 
     }
     
