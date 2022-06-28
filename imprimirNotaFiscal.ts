e276603b206320c05d0395f81f06cc881596d9a8
LourdesDeAssis <100160739+LourdesDeAssis@users.noreply.github.com>
2022-06-28 17:59:15 -0300

    Update imprimirNotaFiscal.ts

--- imprimirNotaFiscal.ts
+++ imprimirNotaFiscal.ts
@@ -1,19 +1,99 @@
-imprimirNotaFiscal(): void {
-    var data = new Date();
-    console.log("--------------------------------------------------------------------------------------------------------------------")
-    console.log("NOTA FISCAL                                                                                                   ", data)
-    console.log("Cliente:",this.cliente.getcodigo(), "      Nome:",this.cliente.getnome() )
-    console.log("CPF:",this.cliente.getcnpjcpf())
-    console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    console.log("ITENS")
-    console.log("--------------------------------------------------------------------------------------------------------------------")
-    console.log("Seq    descrição                                                               QTD        Preco UNIT        Preço   ")
-    console.log("------------------------------------------------------------------------      -----     -------------     ----------")
-    for(let count = 0; count < this.items.length; count++){
-        let precoTotal =  this.items[count].getquant() * this.items[count].produto.getprecoUnico()
-        this.precoTotal += precoTotal
-        console.log("%i       %s                                                       %f            %f              %f  ",
-        this.items[count].getsequencial(), this.items[count].produto.getdescricao(), this.items[count].getquant(), this.items[count].produto.getprecoUnico(), precoTotal)
+import {Cliente} from "./cliente";
+import {ItemNotaFiscal} from "./item_nota_fiscal";
+export class NotaFiscal {
+
+    /* Atributos */
+    id: number;
+    codigo:number; 
+    data: Date;
+    cliente: Cliente;
+    items :  Array<ItemNotaFiscal>;  
+    valorNota : number;
+
+
+    /* Método Construtor */    
+    constructor (id : number, codigo: number, cliente : Cliente ) {
+        this.id = id;
+        this.codigo = codigo;
+        this.data = new Date(); 
+        this.cliente = cliente; 
+        this.valorNota =0.0;
+        this.items = new Array<ItemNotaFiscal>();
+
+
     }
-    console.log("")
+
+    /* Métodos Acessores */
+    getid(): number {
+        return this.id;
+    }
+
+    getcodigo(): number {
+        return this.codigo;
+    }
+
+    getdata(): Date {
+        return this.data;
+    }
+
+    /* Métodos Modificadores */     
+    setcodigo(codigo: number): void {
+        this.codigo = codigo;
+    }
+
+    setdata(data : Date) : void {
+        this.data = data;
+    }
+
+     
+
+    adicionarItem(item: ItemNotaFiscal) {
+        this.items.push(item);
+        
+        this.valorNota += item.valor;
+    }
+
+    
+    calcularValorNotaFiscal() : number {
+        let valorCalculado = 0;
+
+
+        return valorCalculado;
+
+    }
+
+   
+    
+    imprimirNotaFiscal(): void {
+        var data = new Date();
+        console.log("--------------------------------------------------------------------------------------------------------------------")
+        console.log("NOTA FISCAL                                                                                                   ", data)
+        console.log("Cliente:",this.cliente.getcodigo(), "      Nome:",this.cliente.getnome() )
+        console.log("CPF:",this.cliente.getcnpjcpf())
+        console.log("--------------------------------------------------------------------------------------------------------------------")
+        console.log("ITENS")
+        console.log("--------------------------------------------------------------------------------------------------------------------")
+        console.log("Seq    Descrição                                                               QTD       Valor Unit         Preço   ")
+        console.log("------------------------------------------------------------------------      -----     -------------     ----------")
+        for(let count = 0; count < this.items.length; count++){
+            let precoTotal =  this.items[count].getquantidade() * this.items[count].produto.getvalorUnitario()
+            this.valorNota += precoTotal
+            console.log("%i       %s                                                       %f            %f              %f  ",
+            this.items[count].getsequencial(), this.items[count].produto.getdescricao(), this.items[count].getquantidade(), this.items[count].produto.getvalorUnitario(), precoTotal)
+        }
+        console.log("--------------------------------------------------------------------------------------------------------------------")
+        console.log("Valor Total:", this.valorNota)
+    }
+
+    SomaTotal(): void {
+        let soma = 0
+        for(let count = 0; count < this.items.length; count++){
+            let quantia =  this.items[count].getquantidade() * this.items[count].getvalor()
+            soma += quantia
+        }
+
+        console.log("A QUANTIA EM DINHEIRO TOTAL DA SUA COMPRA: R$ " + soma)
+    }
+
+    
 }
